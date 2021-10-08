9e4d19631118eb70c6eef43a2edca005cf1ccd7a
YPgN <90779741+YPgN@users.noreply.github.com>
2021-10-08 18:43:11 +0100

    Create a1_pizza_party.js

---
+++ a1_pizza_party.js
@@ -0,0 +1,55 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>A1 - Pizza Party</title>
+  <link rel="stylesheet" href="a1_pizza_party.css" />
+</head>
+
+<body>
+  <div class="transition transition-1 is-active"></div>
+  <div class="page">
+    <header>
+      <nav>
+        
+        <a href="index.html"><img src="file:///C:/Users/User/OneDrive/Escritorio/STUDY%20WORKSPACE/COMPUTING/2021-2022/YiPeng%20(2)/Images/YPgN-logo.png" alt="YPgN Logo" class="logo" /></a>
+        
+        <ul class="menu">
+          <li class="menu-item"><a href="index.html">HOME</a></li>
+          <li class="menu-item"><a href="Database.html">DATABASE</a></li>
+          <li class="menu-item"><a href="Web.html">WEB</a></li>
+          <li class="menu-item"><a href="Security.html">SECURITY</a></li>
+          <li class="menu-item"><a href="Contact.html">CONTACT</a></li>
+        </ul>
+
+      </nav>
+    </header>
+
+    <main>
+      <section class="banner">
+        <div class="banner-inner">
+          <h1 class="main-title">A1 - Pizza Party</h1>
+        </div>
+      </section>
+
+      <section class="page-content">
+        <div class="content-liner">
+          <h2 class="underline">A1 - PIZZA PARTY</h2>
+          <p style="font-family: 'Courier New', Courier, monospace; text-align: left;">
+            <p>DO YOU LIKE PIZZA? CLICK THE LINK BELOW IF YOU DO!</p>
+            <p>
+              <span style="color: red;">ALERT!!!:</span>
+              <span>IT IS A SURVEY</span>
+            </p>
+            <p style="border: blue;"><a class="link" href="https://forms.gle/FpF8kSeiKjyuQ4vf6">A1 - Pizza Party</a></p>
+          </p>
+        </div>
+      </section>
+    </main>
+  </div>
+
+  <script src="main.js"></script>
+
+</body>
+</html>
